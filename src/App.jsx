c0d2f385356eed4a515defeb1e3d185dd329c7a6
phptuts <glaserpower@gmail.com>
2023-09-11 11:08:14 -0700

    5-event-listeners

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,27 @@ function App() {
   let name = "Noah";
   let colors = ["red", "yellow", "green", "blue", "red"];
   let favoriteNumbers = [7, 77, 43, 90, 81];
+  function hi() {
+    alert("hi");
+  }
+  function onLeaveFavoriteColor() {
+    console.log("leaving favorite colors");
+  }
   return (
     <>
       <h1>My favorite color is: {color}</h1>
       <h2>hi, {name}</h2>
-      <ol>
+      <ol onMouseOver={hi}>
         {favoriteNumbers.map((n) => {
           return <li key={n}>{n}</li>;
         })}
       </ol>
-      <ul>
+      <ul onMouseLeave={onLeaveFavoriteColor}>
         {colors.map((c, index) => {
           return <li key={index}>{c}</li>;
         })}
       </ul>
+      <button onClick={hi}>Hi</button>
     </>
   );
 }
