7e30b529ec7c2aee1bffe59f0ec86a285faf07e9
phptuts <glaserpower@gmail.com>
2023-09-11 11:03:11 -0700

    4-jsx-arrays

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,22 @@ import "./App.css";
 function App() {
   let color = "green";
   let name = "Noah";
+  let colors = ["red", "yellow", "green", "blue", "red"];
+  let favoriteNumbers = [7, 77, 43, 90, 81];
   return (
     <>
       <h1>My favorite color is: {color}</h1>
       <h2>hi, {name}</h2>
+      <ol>
+        {favoriteNumbers.map((n) => {
+          return <li key={n}>{n}</li>;
+        })}
+      </ol>
+      <ul>
+        {colors.map((c, index) => {
+          return <li key={index}>{c}</li>;
+        })}
+      </ul>
     </>
   );
 }
