7e99c6d0d53a52e413bf89c396aef9c4b27ef68e
phptuts <glaserpower@gmail.com>
2023-09-16 18:47:54 -0700

    7-use-state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import Example from "./Example";
 import Fun from "./Fun";
 
 function App() {
-  let color = "green";
+  let colorState = useState("green");
+  let color = colorState[0];
+  let setColor = colorState[1];
+  const [favoriteNumber, setFavoriteNumber] = useState(7);
   let name = "Noah";
   let colors = ["red", "yellow", "green", "blue", "red"];
   let favoriteNumbers = [7, 77, 43, 90, 81];
   function hi() {
-    //alert("hi");
+    setColor("blue" + Math.random());
+  }
+  function favNumber() {
+    setFavoriteNumber(11);
   }
   function onLeaveFavoriteColor() {
     console.log("leaving favorite colors");
@@ -16,9 +23,10 @@ function App() {
   return (
     <>
       <h1>My favorite color is: {color}</h1>
+      <h2>Favorite Number {favoriteNumber}</h2>
       <h2>hi, {name}</h2>
-      <Fun name={name} />
-      <Example color={name} />
+      <Fun name={color} />
+      <Example color={color} />
       <ol onMouseOver={hi}>
         {favoriteNumbers.map((n) => {
           return <li key={n}>{n}</li>;
@@ -29,6 +37,7 @@ function App() {
           return <li key={index}>{c}</li>;
         })}
       </ul>
+      <button onClick={favNumber}>Change Number</button>
       <button onClick={hi}>Hi</button>
     </>
   );
