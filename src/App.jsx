b86771f89091f0f02e0ff0225074d15061d0cf34
phptuts <glaserpower@gmail.com>
2023-09-23 21:37:22 -0700

    6-useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Child from "./Child";
 import Color from "./Color";
@@ -6,13 +6,37 @@ import Color from "./Color";
 function App() {
   const [size, setSize] = useState(20);
   const [color, setColor] = useState("#000");
+  const [win, setWin] = useState(false);
   function changeSize(newSize) {
     setSize(newSize);
+  }
+  const [seconds, setSeconds] = useState(0);
+  useEffect(() => {
+    const id = setInterval(() => {
+      setSeconds((s) => {
+        return s + 1;
+      });
+    }, 1000);
+    return () => {
+      clearInterval(id);
+      console.log("unloading");
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log("this changed");
+    if (color == "#000800" && size == 30) {
+      setWin(true);
+    }
+  }, [size, color]);
+
+  if (win) {
+    return <h1>You Won</h1>;
   }
 
   return (
     <>
-      <h2 style={{ fontSize: `${size}px`, color: color }}>Parent</h2>
+      <h2 style={{ fontSize: `${size}px`, color: color }}>{seconds}</h2>
       <Child sizeFunc={changeSize} />
       <Color setColor={setColor} />
     </>
