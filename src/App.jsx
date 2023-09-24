89fbe3446b1e54e811d127205c86f30f5c8bdbc6
phptuts <glaserpower@gmail.com>
2023-09-23 19:08:39 -0700

    2-jsx-styles

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,16 @@ function App() {
   let setColor = colorState[1];
   const [favoriteNumber, setFavoriteNumber] = useState(7);
   const [show, setShow] = useState(true);
+  const [rotateDegrees, setRotateDegrees] = useState(0);
   let name = "Noah";
   let colors = ["red", "yellow", "green", "blue", "red"];
   let favoriteNumbers = [7, 77, 43, 90, 81];
+  const ulStyles = {
+    color: "red",
+    backgroundColor: "blue",
+  };
   function hi() {
-    setColor("blue" + Math.random());
+    setColor("red");
   }
   function favNumber() {
     setFavoriteNumber(11);
@@ -26,11 +31,15 @@ function App() {
   function onLeaveFavoriteColor() {
     console.log("leaving favorite colors");
   }
+
   return (
     <>
       <h1>My favorite color is: {color}</h1>
-      <h2>Favorite Number {favoriteNumber}</h2>
+      <h2 style={{ color: show ? "red" : "green" }}>
+        Favorite Number {favoriteNumber}
+      </h2>
       <h2>hi, {name}</h2>
+      <h2>Rotate H2</h2>
       {show && <p>Hi this is my paragraph</p>}
       <Fun name={color} />
       <Example color={color} />
@@ -39,7 +48,10 @@ function App() {
           return <li key={n}>{n}</li>;
         })}
       </ol>
-      <ul onMouseLeave={onLeaveFavoriteColor}>
+      <ul
+        style={{ color: "green", backgroundColor: color }}
+        onMouseLeave={onLeaveFavoriteColor}
+      >
         {colors.map((c, index) => {
           return <li key={index}>{c}</li>;
         })}
